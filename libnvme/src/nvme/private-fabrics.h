59506496dab2a05be29a2c38b0d5e98601fdddf3
Codacy Archive <codacy-archive@local>
2026-04-21 00:17:40 +0000

    snapshot 2026-04-21T00:17:40.777106+00:00

--- libnvme/src/nvme/private-fabrics.h
+++ libnvme/src/nvme/private-fabrics.h
@@ -78,9 +78,9 @@ struct libnvmf_context {
  * rest of the fabrics layer.
  */
 
-struct libnvmf_discovery_args { /*!generate-accessors*/
-	int max_retries;
-	__u8 lsp;
+struct libnvmf_discovery_args { // !generate-accessors !generate-lifecycle
+	int max_retries; // !default:6
+	__u8 lsp;        // !default:NVMF_LOG_DISC_LSP_NONE
 };
 
 /**
@@ -95,7 +95,7 @@ struct libnvmf_discovery_args { /*!generate-accessors*/
  * @fragment:		Optional fragment identifier component
  *			(separated by '#')
  */
-struct libnvmf_uri { //!generate-accessors
+struct libnvmf_uri { // !generate-accessors
 	char *scheme;
 	char *protocol;
 	char *userinfo;
